ba5112ddbfa06c7dede8e0ab80071b15223f7c55
Diego Gómez <2833316742101@ingenieria.usac.edu.gt>
2026-08-23 14:15:06 -0600

    Estructura MVC agregada

--- Proyecto1/src/controller/Main.java
+++ Proyecto1/src/controller/Main.java
@@ -1,9 +1,9 @@
+package controller;
+
 import java.util.Scanner;
 public class Main{
     public static Scanner read = new Scanner(System.in);
     public static void main(String[] args){
-        System.out.println("Bienvenido");
-        log login = new log();
-        login.password=read.nextLine();
+
     }
 }
