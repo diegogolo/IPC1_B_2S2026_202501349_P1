51df803f1eaa9516db25104c1b8e46bf27490719
Diego Gómez <2833316742101@ingenieria.usac.edu.gt>
2026-08-23 22:27:29 -0600

    arreglos al mvc

--- Proyecto1/src/controller/MainController.java
+++ Proyecto1/src/controller/MainController.java
@@ -1,30 +1,18 @@
 package controller;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import model.MainModel;
+import view.MainView;
 public class MainController {
-    public static Scanner read = new Scanner(System.in);
     public static void main(String[] args){
-        String password=read.nextLine();
-        String[] pass = new String[2];
-        try{
-            BufferedReader br = new BufferedReader(new FileReader("Proyecto1/src/controller/users.txt"));
-            for(int i=0; i<2; i++){
-                pass[i] = br.readLine();
-                if(pass[i].equals(password)){
-                    System.out.println("Correcto");
-                    break;
-                }else{
-                    System.out.println("Incorrecto");
-                }
-            }
-        }catch(FileNotFoundException e){
-            System.out.println(e.getMessage());
+        MainModel modelo = new MainModel();
+        MainView view = new MainView();
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        String key = view.password();
+        boolean pasa = modelo.user(key);
+
+        if (pasa){
+            view.showMensaje("Correcto");
+        }else{
+            view.showMensaje("Incorrecto");
         }
     }
 }
--- Proyecto1/src/view/MainView.java
+++ Proyecto1/src/view/MainView.java
@@ -1,5 +1,12 @@
 package view;
-
+import java.util.Scanner;
 public class MainView {
+    private Scanner read = new Scanner(System.in);
 
+    public String password(){
+        return read.nextLine();
+    }
+    public void showMensaje(String mensaje){
+        System.out.println(mensaje);
+    }
 }
