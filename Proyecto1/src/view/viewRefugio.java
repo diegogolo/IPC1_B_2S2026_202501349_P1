c1f1a28494e143ffdaf59220f89b5103b1caff52
Diego Gómez <2833316742101@ingenieria.usac.edu.gt>
2026-09-03 17:41:00 -0600

    panel de login

--- Proyecto1/src/view/viewRefugio.java
+++ Proyecto1/src/view/viewRefugio.java
@@ -1,7 +1,23 @@
 package view;
+import java.awt.*;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class viewRefugio {
-    private Scanner read = new Scanner(System.in);
-    
+    final private Scanner read = new Scanner(System.in);
+    JFrame frame = new JFrame();
+    JPasswordField pass;
+    public viewRefugio(){
+        frame.setTitle("Login");
+        frame.setSize(600,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(2,2));
+        JLabel tag = new JLabel("Contraseña");
+        pass = new JPasswordField();
+        JButton boton = new JButton("Ingresar");
+        frame.add(tag);
+        frame.add(pass);
+        frame.add(boton);
+        frame.setVisible(true);
+    }
 }
